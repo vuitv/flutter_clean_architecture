4158921096ef39f00d48982db138d1878b489f87
vuitv <tangvuicntt@gmail.com>
2021-09-25 21:50:14 +0700

    init

--- data/test/data_source/remote/screen_setting_api_test.dart
+++ data/test/data_source/remote/screen_setting_api_test.dart
@@ -1,10 +1,8 @@
 // import 'package:data/src/data_source/remote/base/api_client.dart';
-// import 'package:data/src/data_source/remote/base/log_api_client.dart';
 // import 'package:dio/dio.dart';
 // import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  // const String baseUrl = 'https://api-staging-beemart.sapoapps.vn/api';
   // late Dio dio;
   // late LogApiService log;
   // late ApiClient client;
